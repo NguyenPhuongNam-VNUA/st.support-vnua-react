a4addc838b2a84458484495295c1097c32d2566f
Nguyễn Phương Nam <namdev@MacBook-Pro-cua-Nguyen.local>
2025-07-18 17:20:52 +0700

    add margin icon new chat

--- src/pages/chatBot/chat-bot.jsx
+++ src/pages/chatBot/chat-bot.jsx
@@ -178,7 +178,7 @@ export default function ChatBot() {
         <Box px={3} py={2} borderTop="1px solid #e0e0e0">
           <FlexBetween>
             <Tooltip title="Mở hội thoại mới" placement="top">
-            <IconButton onClick={() => setMessages([])}>
+            <IconButton onClick={() => setMessages([])} sx={{ marginRight: 2, marginLeft: -1 }}>
               <BorderColorIcon variant="contained" color='primary' />
             </IconButton>
             </Tooltip>
